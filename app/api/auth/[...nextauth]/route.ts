ee0f84fff2f0de9301404c1f13922d944587b3f3
grafek <jacek.grafender@gmail.com>
2023-06-11 16:00:50 +0200

    project init and libraries setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import type { DefaultSession, NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/prisma";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
